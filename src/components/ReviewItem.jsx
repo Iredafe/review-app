f495e60d9b987e21d4bbda0adf6d50de00f44604
iredafe owolabi <iredafeowolabi@gmail.com>
2023-01-16 21:54:52 -0700

    WIP

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -1,7 +1,12 @@
 import Card from './shared/Card';
 
 function ReviewItem({ item }) {
-  return <Card rating={item.rating} text={item.text} />;
+  return (
+    <Card>
+      <div className='num-display'>{item.rating}</div>
+      <div className='text-display'>{item.text}</div>
+    </Card>
+  );
 }
 
 export default ReviewItem;
--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,10 +1,5 @@
-function Card({ rating, text }) {
-  return (
-    <div className='card'>
-      <div className='num-display'>{rating}</div>
-      <div className='text-display'>{text}</div>
-    </div>
-  );
+function Card({ children }) {
+  return <div className='card'>{children}</div>;
 }
 
 export default Card;
