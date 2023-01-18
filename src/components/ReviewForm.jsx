7d899dabc811b5514d0b8a7e9b7e53c58609e51f
iredafe owolabi <iredafeowolabi@gmail.com>
2023-01-18 16:08:38 -0700

    adding form

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
+import Card from './shared/Card';
 
 function ReviewForm() {
-  return <div>I am a Form</div>;
+  const [text, setText] = useState('');
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <Card>
+      <form action=''>
+        <h2>How would you rate your experience with our product?</h2>
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            type='text'
+            placeholder='Write a review'
+            value={text}
+          />
+          <button type='submit'>Send</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
 export default ReviewForm;
