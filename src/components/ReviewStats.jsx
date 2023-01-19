e4d70df1c7a1dd470a72ce55fdc91554f97c0c15
iredafe owolabi <iredafeowolabi@gmail.com>
2023-01-18 20:53:44 -0700

    calculate average

--- src/components/ReviewStats.jsx
+++ src/components/ReviewStats.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function ReviewStats({ review }) {
-  return <div className='feedback-stats'></div>;
+  let sumOfRatings = 0;
+  let averageRating = 0;
+  let numberOfReviews = review.length;
+
+  review.forEach((element) => {
+    sumOfRatings += element.rating;
+  });
+  averageRating = sumOfRatings / numberOfReviews;
+  return (
+    <div className='feedback-stats'>
+      <h4>{numberOfReviews} Reviews</h4>
+      <h4>Average Rating: {averageRating}</h4>
+    </div>
+  );
 }
 
 export default ReviewStats;
